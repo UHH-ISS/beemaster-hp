01dee050ec4a51d78c05c574c85bb3b4a0132d71
Hauke <4preuss@informatik.uni-hamburg.de>
2016-12-10 17:17:27 +0100

    Use configuration file for broker endpoint configuration

--- beemaster/connector/sender.py
+++ beemaster/connector/sender.py
@@ -7,21 +7,22 @@ communication partner.
 import pybroker as pb
 
 class Sender(object):
-    def __init__(self, address, port):
+    def __init__(self, address, port, brokerEndpointName, brokerTopic):
         """Sender(address, port)
 
         Initialises the Sender. The address/port are used to peer to the
         corresponding Broker partner.
 
-        :param address:     The address to send to. (str)
-        :param port:        The port to send to. (int)
+        :param address:            The address to send to. (str)
+        :param port:               The port to send to. (int)
+        :param brokerEndpointName: The broker endpoint name to send to. (str)
+        :param brokerTopic:        The broker topic to send to. (str)
         """
-        self.address = address # could be removed and used directly
-        self.port = port
-        #TODO: Move this part into the send method or elsewhere, depending how changing the bro (boker endpoint) should be handled [service discovery]
-        self.dioEp = pb.endpoint("dioEp")
-        # TODO: check connection status: epStatus = self.dioEp.outgoing_connection_status()
-        # peer with broker endpoint. Broker endpoint has to listen to us
+
+        self.brokerTopic = brokerTopic
+        #TODO: expansion version 1: add service discovery
+        self.dioEp = pb.endpoint(brokerEndpointName)
+        # peer with broker endpoint. Broker endpoint has to listen to us to receive messages
         self.dioEp.peer(address, port)
 
         # TODO: in the future: provide a channel to listen, to accept commands, like deactivate file logging etc.
@@ -33,11 +34,9 @@ class Sender(object):
         Sends the msg to the peer.
 
         :param msg:        The message to be sent. (Broker message)
-        :returns:           True if message was successfully sent.
+        :returns:          None
         """
-        print("Sender should send '{}'' to {}:{}"
-              .format(msg, self.address, self.port))
-        #TODO: make topic more dynamic? dionaea-ID? Or send id with the broker message?
-        self.dioEp.send("honeypot/dionaea/", msg)
 
-        return True #TODO: Is a check possible (message sent & received)? Perhaps by using connection status?
+        self.dioEp.send(self.brokerTopic, msg)
+
+        return
