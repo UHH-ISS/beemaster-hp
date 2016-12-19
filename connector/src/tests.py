457e88a6a79a45dc819543f1f712f7585f68e684
Jens Neumann <5jneuman@informatik.uni-hamburg.de>
2016-12-19 23:24:16 +0100

    Tests hinzugefügt iss/mp-ids#87

--- connector/src/tests.py
+++ connector/src/tests.py
@@ -23,6 +23,13 @@ class TestMapper(unittest.TestCase):
                                      "local_ip": "127.0.0.1", "remote_port":
                                      35324, "transport": "tcp"}}, "name":
                      "dionaea", "origin": "dionaea.connection.free"}
+    VALID_INPUT_2 = {"timestamp": "2016-11-26T22:18:56.281464", "data":
+                     {"connection": {"remote_ip": "2001:0:509c:564e:34ae:3a9a:3f57:fd91",
+                                     "remote_hostname": "", "id": 3019197952,
+                                     "protocol": "pcap", "local_port": 4101,
+                                     "local_ip": "127.0.0.1", "remote_port":
+                                     35324, "transport": "tcp"}}, "name":
+                     "dionaea", "origin": "dionaea.connection.free"}
     INVALID_INPUT_1 = {"origin": "dionaea.connection.link", "timestamp":
                        "2016-12-09T21:11:09.315143",
                        "data": {"parent": {"protocol": "httpd", "local_port":
@@ -38,6 +45,27 @@ class TestMapper(unittest.TestCase):
                                           "transport": "tcp", "remote_ip":
                                           "127.0.0.1"}},
                        "name": "dionaea"}
+    INVALID_INPUT_2 = {"timestamp": "2016-11-26T22:18:56.281464", "data":
+                       {"connection": {"remote_ip": "12.12.12.12.12.12",
+                                       "remote_hostname": "", "id": 3019197952,
+                                       "protocol": "pcap", "local_port": 4101,
+                                       "local_ip": "127.0.0.1", "remote_port":
+                                       35324, "transport": "tcp"}}, "name":
+                       "dionaea", "origin": "dionaea.connection.free"}
+    INVALID_INPUT_3 = {"timestamp": "2016-11-26T22:18:56.281464", "data":
+                       {"connection": {"remote_ip": "127.0.0.1",
+                                       "remote_hostname": "", "id": 3019197952,
+                                       "protocol": "pcap", "local_port": "4101",
+                                       "local_ip": "127.0.0.1", "remote_port":
+                                       35324, "transport": "tcp"}}, "name":
+                       "dionaea", "origin": "dionaea.connection.free"}
+    INVALID_INPUT_4 = {"timestamp": "2016-11-26T22:18:56.281464", "data":
+                       {"connection": {"remote_ip": "12...12",
+                                       "remote_hostname": "", "id": 3019197952,
+                                       "protocol": "pcap", "local_port": 4101,
+                                       "local_ip": "127.0.0.1", "remote_port":
+                                       35324, "transport": "tcp"}}, "name":
+                       "dionaea", "origin": "dionaea.connection.free"}
 
     VALID_MAPPING = 'mappings/dionaea/connection.yaml'
 
@@ -52,7 +80,7 @@ class TestMapper(unittest.TestCase):
         def _map_time(inp):
             date = datetime.strptime(inp, '%Y-%m-%dT%H:%M:%S.%f')
             return pb.time_point((date - datetime.utcfromtimestamp(0))
-                                 .total_seconds() * 1000.0)
+                                 .total_seconds())
 
         def _map_addr(inp):
             return pb.address_from_string(inp)
@@ -66,13 +94,52 @@ class TestMapper(unittest.TestCase):
             message.append(i)
 
         result = self.mapper.transform(self.VALID_INPUT_1)
-
+        #while not message.empty():
+        #    print(message.pop())
+        #    print(result.pop())
         while not message.empty():
-            self.assertEqual(message.pop(), result.pop())
+            self.assertEqual(str(message.pop()), str(result.pop()))
 
-    def testFailure(self):
+    def testDefaultSuccess2(self):
+        """Test the default successful scenario."""
+        def _map_time(inp):
+            date = datetime.strptime(inp, '%Y-%m-%dT%H:%M:%S.%f')
+            return pb.time_point((date - datetime.utcfromtimestamp(0))
+                                 .total_seconds())
+
+        def _map_addr(inp):
+            return pb.address_from_string(inp)
+
+        expect = map(pb.data, (_map_time("2016-11-26T22:18:56.281464"),
+                               3019197952, _map_addr("127.0.0.1"), 4101,
+                               _map_addr("2001:0:509c:564e:34ae:3a9a:3f57:fd91"), 35324, "tcp"))
+
+        message = pb.message()
+        for i in expect:
+            message.append(i)
+
+        result = self.mapper.transform(self.VALID_INPUT_2)
+        #while not message.empty():
+        #    print(message.pop())
+        #    print(result.pop())
+        while not message.empty():
+            self.assertEqual(str(message.pop()), str(result.pop()))
+
+    def testFailureUnexpectedContent(self):
         """Test empty output on an non-matching message."""
         self.assertIsNone(self.mapper.transform(self.INVALID_INPUT_1))
+
+    def testFailureIPTooLong(self):
+        """Test empty output on an non-matching message."""
+        self.assertIsNone(self.mapper.transform(self.INVALID_INPUT_2))
+
+    def testFailureCountIsString(self):
+        """Test empty output on an non-matching message."""
+        self.assertIsNone(self.mapper.transform(self.INVALID_INPUT_3))
+
+    def testFailureIPMissingBytes(self):
+        """Test empty output on an non-matching message."""
+        self.assertIsNone(self.mapper.transform(self.INVALID_INPUT_4))
 
     def testMissingField(self):
         """Test empty output on message, missing a field."""
