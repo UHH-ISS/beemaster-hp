f3e548f89e46d91a51e5936fb1e7dcbc16910d82
Florian Wilkens <gh@1wilkens.org>
2017-01-19 21:14:10 +0100

    Added Dockerfile_Pi and changed run script to use it

--- run_dio_pi.sh
+++ run_dio_pi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/docker build /home/beemaster/mp-ids/mp-ids-hp/dionaea/ -t dio-local
+/usr/bin/docker build -f /home/beemaster/mp-ids/mp-ids-hp/dionaea/Dockerfile_Pi -t dio-local
 /usr/bin/docker run -p 21:21 -p 80:80 -p 443:443 -p 3306:3306 -p 445:445 --name dio --rm dio-local
